9d5c4ab7a2227d1c8b1b0bf723c7d9aec05c04bf
SlartiBartFast-art <JekHamond@tutanota.com>
2021-02-21 18:10:47 +0500

    5.2.1. Реализовать SimpleArray<T> [#471693]4

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,48 +1,37 @@
 package ru.job4j.generics;
 
+import java.util.*;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
-public class SimpleArray<T> implements Iterator<T> {
-    private final T[] array;
-    private int size = 0;
+public class SimpleArray<T> implements Iterable<T> {
+    private T[] array;
     private int point = 0;
 
-    public SimpleArray(T[] array) {
-        this.array = array;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != null || array[i] != (Integer) 0 || array[i] != (Boolean) false) {
-                size++;
-            }
-        }
-    }
-
-    public int getSize() {
-        return size;
+    public SimpleArray(int length) {
+       // Objects.checkIndex(index, point);
+       this.array = (T[]) new Object[length];
     }
 
     /**
      * добавляет указанный элемент (model) в первую свободную ячейку;
      */
     public void add(T model) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = model;
-                size++;
-                break;
-            }
+        if (Objects.checkIndex(point, array.length) == point) {
+        this.array[point++] = model;
         }
+
     }
 
     /**
      * возвращает элемент, расположенный по указанному индексу;
      */
     public T get(int index) {
-        System.out.println(size + "razmer method get");
-        System.out.println(Objects.checkIndex(index, size) + "result");
-        Objects.checkIndex(index, size);
+        System.out.println(point + "razmer method get");
+        System.out.println(Objects.checkIndex(index, point) + "result");
+        Objects.checkIndex(index, point);
         return array[index];
     }
 
@@ -51,7 +40,7 @@ public class SimpleArray<T> implements Iterator<T> {
      * находящийся по индексу index;
      */
     public void set(int index, T model) { // заменить на элемент
-        Objects.checkIndex(index, size);
+        Objects.checkIndex(index, point);
         array[index] = model;
     }
 
@@ -61,20 +50,10 @@ public class SimpleArray<T> implements Iterator<T> {
      * (в середине массива не должно быть пустых ячеек);
      */
     public void remove(int index) { // то что в ячейке
-        Objects.checkIndex(index, size);
-        System.arraycopy(array, index + 1, array, index, size - (index + 1));
-        array[size - 1] = null;
-        size--;
-    }
-
-    @Override
-    public T next() {
-        return array[point++];
-    }
-
-    @Override
-    public boolean hasNext() {
-        return point < size;
+        Objects.checkIndex(index, point);
+        System.arraycopy(array, index + 1, array, index, point - (index + 1));
+        array[point - 1] = null;
+        point--;
     }
 
     @Override
@@ -83,4 +62,38 @@ public class SimpleArray<T> implements Iterator<T> {
                 + "array=" + Arrays.toString(array)
                 + '}';
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private  int count = 0;
+
+            @Override
+            public boolean hasNext() {
+                return count < point;
+            }
+
+            @Override
+            public T next() {
+                return array[count++];
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+SimpleArray<Integer> simpleArray = new SimpleArray<>(10);
+        System.out.println(simpleArray);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
+        simpleArray.add(2);
+        simpleArray.add(7);
+        System.out.println(simpleArray);
+        simpleArray.set(2, 5);
+        System.out.println(simpleArray);
+        simpleArray.remove(4);
+        System.out.println(simpleArray);
+        System.out.println(simpleArray.point);
+    }
 }
+
--- src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -1,65 +1,54 @@
 package ru.job4j.generics;
 
 import org.junit.Test;
-
 import java.util.Arrays;
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
-
 public class SimpleArrayTest {
 
-    @Test
+   @Test
     public void add() {
-        SimpleArray<Integer> array = new SimpleArray<>(new Integer[3]);
-        array.add(7);
-        array.add(2);
-        SimpleArray<Integer> exp = new SimpleArray<>(new Integer[]{7, 2, 0});
-        assertThat(exp.toString(), is("SimpleArray{"
-                + "array=" + "[" + "7," + " " + "2," + " " + "0" + "]"
+        SimpleArray<Integer> simpleArray = new SimpleArray(3);
+        simpleArray.add(7);
+        simpleArray.add(2);
+        SimpleArray<Integer> rsl = simpleArray;
+        assertThat(rsl.toString(), is("SimpleArray{"
+                + "array=" + "[" + "7," + " " + "2," + " " + "null" + "]"
                 + '}'));
     }
 
     @Test
     public void get() {
-        SimpleArray<Integer> array = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
-        int rsl = array.get(2);
-        assertThat(rsl, is (3));
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(4);
+        simpleArray.add(7);
+        simpleArray.add(8);
+        simpleArray.add(15);
+        int rsl = simpleArray.get(2);
+        assertThat(rsl, is (15));
     }
 
     @Test
     public void set() {
+       SimpleArray<Integer> simpleArray = new SimpleArray<>(4);
+       simpleArray.add(1);
+       simpleArray.add(2);
+       simpleArray.add(3);
+       simpleArray.set(0, 7);
+       assertThat(simpleArray.get(0), is(7));
     }
 
     @Test
     public void remove() {
-        SimpleArray<Integer> array = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
-        array.remove(1);
-        SimpleArray<Integer> rsl = array;
-        SimpleArray<Integer> exp = new SimpleArray<>(new Integer[]{1, 3, 4, null});
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(4);
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
+        simpleArray.remove(1);
+        SimpleArray<Integer> rsl = simpleArray;
+        //SimpleArray<Integer> exp = new SimpleArray<Integer>(new int[]{1, 0, 3, 0, 0});
         assertThat(rsl.toString(), is ("SimpleArray{"
-                + "array=" + "[" + "1," + " " + "3," + " " + "4," + " " + "null" + "]"
+                + "array=" + "[" + "1," + " " + "3," + " " + "null," + " " + "null" + "]"
                 + '}'));
-    }
-    @Test
-    public void whenReadInteger() {
-        SimpleArray<Integer> it = new SimpleArray<>(new Integer[] {1, 2, 3}
-        );
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));
-    }
-    @Test
-    public void whenMultiCallhasNextThenTrue() {
-        SimpleArray<Integer> it = new SimpleArray<>(new Integer[] {1, 2, 3}
-        );
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(2));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(3));
-        assertThat(it.hasNext(), is(false));
     }
 
 }
