76b3ef51b38f60e8af3b5402bd509145132515b8
SlartiBartFast-art <JekHamond@tutanota.com>
2021-04-21 19:16:27 +0500

    0.4. BufferedOutputStream [#471722]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,9 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +8,16 @@ import java.util.List;
  * 0.3. BufferedReader. [#471721]
  * Создайте файл log.txt. Запишите в него текст.
  * Метод filter должен прочитать файл и вернуть строки, где предпоследнее число - это 404.
+ * 0.4. BufferedOutputStream [#471722]
+ * Нужно добавить метод save(String log). Метод должен записывать результат фильтрации в файл
  */
 public class LogFilter {
+
+    /**
+     * Метод должен прочитать файл и вернуть строки, где предпоследнее число - это 404.
+     * @param file название файла где храняться исходные данне
+     * @return возвращает коллекцию с результатами работы метода
+     */
     public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
@@ -25,8 +30,28 @@ public class LogFilter {
         return list;
     }
 
+    /**
+     * Метод записывает результаты фильтрации в файл
+     * @param log название файла в коротом нужно провести фильтрацию
+     * @return название файла в который будут записаны результаты фильтрации
+     */
+    public static String save(String log) {
+        String string;
+        List<String> saveLog;
+        saveLog = filter(log);
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream("writeLog.txt")))) {
+            saveLog.forEach(x -> printWriter.printf("%s%n", x));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        string = "writeLog.txt";
+        return string; // записать название файла
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        String string = save("log.txt");
     }
 }
