9a46d2041ee812630250560a39864647252ddeb5
SlartiBartFast-art <JekHamond@tutanota.com>
2021-04-21 14:21:41 +0500

    0.2. FileInputStream [#471730] #1

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,28 +7,29 @@ import java.util.Scanner;
 
 /**
  * 0.2. FileInputStream [#471730]
- *  В классе нужно прочитать файл even.txt.
- *  В файл добавьте числа. Одно число  на строку.
- *  Для каждого числа проверить является ли оно четным числом.
- *  Ответ вывести на консоль.
+ * В классе нужно прочитать файл even.txt.
+ * В файл добавьте числа. Одно число  на строку.
+ * Для каждого числа проверить является ли оно четным числом.
+ * Ответ вывести на консоль.
  */
 public class EvenNumberFile {
-    public static void main(String[] args) throws FileNotFoundException {
-        var file = new FileInputStream("even.txt");
-        try (var out = new Scanner(file)) {
+    public static void main(String[] args) {
+
+        try (var out = new Scanner(new FileInputStream("even.txt"))) {
             int rsl;
             while (out.hasNext()) {
                 rsl = out.nextInt();
                 if (rsl % 2 == 0) {
                     System.out.println("Это число является четным :" + rsl);
                 } else {
-                    System.out.println("Это число не четное : " + rsl);
+                    System.out.println("Это число нечетное : " + rsl);
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
-
 
 
 
