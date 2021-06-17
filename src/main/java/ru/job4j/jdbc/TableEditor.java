4e1a589e25f20294d72a0c61c52d21c7f7d6e53a
SlartiBartFast-art <JekHamond@tutanota.com>
2021-06-17 20:53:46 +0500

    0.1. Statement [#379306 #168426] №2

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -2,6 +2,7 @@ package ru.job4j.jdbc;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
@@ -9,8 +10,8 @@ import java.util.Properties;
 /**
  * Топик : 2.3.4. JDBC
  * 0.1. Statement [#379306]
- *  Дан каркас класса. Реализовать его методы
- *  Чтение настроек должно идти из файла *.properties
+ * Дан каркас класса. Реализовать его методы
+ * Чтение настроек должно идти из файла *.properties
  */
 public class TableEditor implements AutoCloseable {
     private Connection connection; //соединение
@@ -21,48 +22,50 @@ public class TableEditor implements AutoCloseable {
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
-    }
-
     /**
-     * Метод создает подключение по ключ\значение
-     * @return объект Connection
-     * @throws ClassNotFoundException
-     * @throws SQLException
+     * Метод проводит чтение данных из файла *.properties
+     * прописывает необходимые значения(key-value) для установки соединения
+     * говорит какой именно драйвер необходимо загрузить
+     * проводит загрузку драйвера
      */
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
+    private void initConnection() {
         String driver = properties.getProperty("hibernate.connection.driver_class");
-        Class.forName(driver);
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oracle JDBC Driver is not found");
+            e.printStackTrace();
+        }
         String url = properties.getProperty("hibernate.connection.url");
         String login = properties.getProperty("hibernate.connection.username");
         String password = properties.getProperty("hibernate.connection.password");
-        connection = DriverManager.getConnection(url, login, password);
-        return connection;
+        try {
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     /**
      * Метод устанавливает соединение + создает объект для внесния изминений в базу
      * @param string запрос/действие которое необходимо провести в таблице
-     * @throws SQLException
-     * @throws ClassNotFoundException
      */
-    private void setConnection(String string) throws SQLException, ClassNotFoundException {
+    private void setConnection(String string) {
         if (connection != null) {
-            try (Connection connection = getConnection()) {
-                try (Statement statement = connection.createStatement()) {
-                    String sql = string;
-                    System.out.println(sql);
-                    statement.execute(sql);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
+            try (Statement statement = connection.createStatement()) {
+                String sql = string;
+                System.out.println(sql);
+                statement.execute(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
 
     /**
      * Метод производит создание таблицы с указанным именем
+     *
      * @param tableName имя таблицы которую мы создаем
      * @throws SQLException
      * @throws ClassNotFoundException
@@ -76,7 +79,8 @@ public class TableEditor implements AutoCloseable {
 
     /**
      * Меод производит удаление таблицы из БД
-     * @param tableName
+     *
+     * @param tableName имя таблицы для удаления
      * @throws SQLException
      * @throws ClassNotFoundException
      */
@@ -89,23 +93,25 @@ public class TableEditor implements AutoCloseable {
 
     /**
      * Метод производит добавление колонки в таблицу
-     * @param tableName имя таблицы куда производится добавление колонки
+     *
+     * @param tableName  имя таблицы куда производится добавление колонки
      * @param columnName имя колонки
-     * @param type параметр хранимого занчения
+     * @param type       параметр хранимого занчения
      * @throws SQLException
      * @throws ClassNotFoundException
      */
     public void addColumn(String tableName, String columnName, String type) throws SQLException, ClassNotFoundException {
         String sql = String.format(
-                "alter table if exists %s add column %s %s;", tableName,
-                columnName, type
+                "alter table if exists %s add column %s %s;",
+                tableName, columnName, type
         );
         setConnection(sql);
     }
 
     /**
      * Метод производит удаление колонки в указываемой таблице
-     * @param tableName таблица в БД в которой производится удаление колонки
+     *
+     * @param tableName  таблица в БД в которой производится удаление колонки
      * @param columnName удаляемая колонка в таблице
      * @throws SQLException
      * @throws ClassNotFoundException
@@ -120,8 +126,8 @@ public class TableEditor implements AutoCloseable {
 
     /**
      * Метод производит изминение/переименование колонки в таблице
-     * @param tableName имя таблицы в БД в которой будет происходит смена колонки
-     * @param columnName имя колонки для переименования
+     * @param tableName     имя таблицы в БД в которой будет происходит смена колонки
+     * @param columnName    имя колонки для переименования
      * @param newColumnName новое имя колонки после переименования
      * @throws SQLException
      * @throws ClassNotFoundException
@@ -142,7 +148,6 @@ public class TableEditor implements AutoCloseable {
      */
     public String getScheme(String tableName) throws SQLException, ClassNotFoundException {
         StringBuilder scheme = new StringBuilder();
-        getConnection();
         DatabaseMetaData metaData = connection.getMetaData();
         try (ResultSet columns = metaData.getColumns(null, null, tableName, "%")) {
             scheme.append(String.format("%-15s %-15s%n", "column", "type"));
@@ -163,20 +168,30 @@ public class TableEditor implements AutoCloseable {
     }
 
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-        Properties properties = new Properties();
-        properties.load(new FileInputStream("connDemoApp.properties"));
-        TableEditor tableEditor = new TableEditor(properties);
-        tableEditor.getConnection();
-        tableEditor.createTable("Warlon");
-        System.out.println(tableEditor.getScheme("warlon"));
-        tableEditor.addColumn("Warlon", "ego", "varchar (50)");
-        System.out.println(tableEditor.getScheme("Warlon"));
-        tableEditor.dropColumn("warlon", "name");
-        System.out.println(tableEditor.getScheme("warlon"));
-        tableEditor.renameColumn("warlon", "ego", "black");
-        System.out.println(tableEditor.getScheme("warlon"));
-        tableEditor.dropTable("warlon");
-        System.out.println(tableEditor.getScheme("warlon"));
 
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream("connDemoApp.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            TableEditor tableEditor = new TableEditor(properties);
+            tableEditor.createTable("warlon");
+            System.out.println(tableEditor.getScheme("warlon"));
+            tableEditor.addColumn("warlon", "name", "varchar (250)");
+            System.out.println(tableEditor.getScheme("warlon"));
+            tableEditor.addColumn("warlon", "ego", "varchar (50)");
+            System.out.println(tableEditor.getScheme("warlon"));
+            tableEditor.dropColumn("warlon", "name");
+            System.out.println(tableEditor.getScheme("warlon"));
+            tableEditor.renameColumn("warlon", "ego", "black");
+            System.out.println(tableEditor.getScheme("warlon"));
+            tableEditor.dropTable("warlon");
+            System.out.println(tableEditor.getScheme("warlon"));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
